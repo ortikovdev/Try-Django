565e8f299f3f8f3d2ed5c34470e80d94b2eca18b
unknown <oranwhatever@gmail.com>
2024-01-05 23:48:39 +0500

    admin panel updated

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from django.contrib import admin
 from .models import (
     Tag,
@@ -6,6 +8,31 @@ from .models import (
 )
 
 
-admin.site.register(Tag)
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+
+
+class IngredientAdminTabluarInline(admin.TabularInline):
+    model = Ingredient
+    extra = 0
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (IngredientAdminTabluarInline, )
+    list_display = ('id', 'title', 'author', 'created_date')
+    search_fields = ('title', 'author__username')
+    list_filter = ('created_date', )
+    autocomplete_fields = ('author',)
+    date_hierarchy = 'created_date'
+    filter_horizontal = ('tags', )
+    readonly_fields = ('created_date',)
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'recipe', 'title', 'quantity', 'unit', 'is_active')
+    search_fields = ('recipe__title', 'title')
+    list_filter = ('is_active', 'unit')
+    autocomplete_fields = ('recipe',)
